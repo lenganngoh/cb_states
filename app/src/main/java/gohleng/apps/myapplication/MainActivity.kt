326f61744e0ae47189dc7382b491c09440e76dce
Leng Ann Goh <lengann@fbmsoftware.com>
2020-07-29 01:28:01 +0800

    Applied and implemented the SELECT ALL checkbox functionality (L1 for the level structure based on Readme)

--- app/src/main/java/gohleng/apps/myapplication/MainActivity.kt
+++ app/src/main/java/gohleng/apps/myapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package gohleng.apps.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import gohleng.apps.myapplication.adapter.CheckboxExpandableListAdapter
+import gohleng.apps.myapplication.adapter.CheckboxExpandableListAdapter.Listener
 import gohleng.apps.myapplication.test.Test
+import gohleng.apps.myapplication.widget.ThreeStateCheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +22,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun initializeListView() {
         val dataKeys = ArrayList(Test.data.keys)
-        adapter = CheckboxExpandableListAdapter(this, dataKeys as ArrayList<String>, Test.data)
+        adapter = CheckboxExpandableListAdapter(this, dataKeys as ArrayList<String>, Test.data,
+            object : Listener {
+                override fun onNotifyDataSetChanged() {
+                    when (adapter!!.getGroupSelectedCountWithIndeterminate()) {
+                        adapter!!.groupCount -> {
+                            cbSelectAll.setState(ThreeStateCheckBox.State.CHECKED)
+                        }
+                        0 -> {
+                            cbSelectAll.setState(ThreeStateCheckBox.State.UNCHECKED)
+                        }
+                        else -> {
+                            cbSelectAll.setState(ThreeStateCheckBox.State.INDETERMINATE)
+                        }
+                    }
+                }
+            }
+        )
         listCheckbox.setAdapter(adapter)
     }
 
